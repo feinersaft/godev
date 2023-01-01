a0399e6b42578602ed2073c33c793b3475216fe7
Christopher Schmeer <78271080+feinersaft@users.noreply.github.com>
2023-01-01 18:22:58 +0100

    Update guess.go

--- guess/guess.go
+++ guess/guess.go
@@ -1,33 +1,26 @@
 package main
 
 import (
-  "math/rand"
-  "fmt"
-  "log"
-  "time"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
-  fmt.Println("Please enter a number (1-100):")
-  var pickedNumber, diff string
-  n, err := fmt.Scanln(&pickedNumber)
-  
-  if err != nil {
-    log.Fatal(err)
-  }
-  
-  var guess int
-  
-  for pickedNumber != guess {
-    rand.Seed(time.Now(),UnixNano())
-    guess = rand.Intn(100)
-    if pickedNumber > guess {
-      diff = "small"
-    } else {
-      diff = "big"
-    }
-    fmt.Printf("My guess was %v and is too %v...\n", guess, diff)
-  }
-  
-  fmt.Printf("I guess your number is %v!\n", guess)
+	fmt.Println("Please enter a number (1-100):")
+	var pickedNumber, guess int
+	fmt.Scanln(&pickedNumber)
+
+	for pickedNumber != guess {
+		rand.Seed(time.Now().UnixNano())
+		guess = rand.Intn(100)
+		if pickedNumber > guess {
+			fmt.Printf("My guess was %v and is too small...\n", guess)
+		} else {
+			fmt.Printf("My guess was %v and is too big...\n", guess)
+		}
+
+	}
+
+	fmt.Printf("I guess your number is %v!\n", guess)
 }
