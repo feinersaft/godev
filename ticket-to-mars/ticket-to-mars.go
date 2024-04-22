053749c5fdfd2b720c310f02f1bde0df5fb43f64
Christopher Schmeer <78271080+feinersaft@users.noreply.github.com>
2024-04-22 20:16:19 +0200

    Update ticket-to-mars.go
    refactor logic

--- ticket-to-mars/ticket-to-mars.go
+++ ticket-to-mars/ticket-to-mars.go
@@ -7,31 +7,58 @@ import (
 
 var oneway bool
 var spaceline string
-var speed int
-var price float32
+var speed, price int
 
 const distance = 62100000
 
-func generateTicket() (string, int) {
+func generateTicket() {
 	speed = rand.Intn(14) + 16
-	//seconds := distance / speed
-	//spaceline := rand.Intn(2)
+	price = (distance / speed) / 60 / 60 / 24
 
 	if rand.Intn(1) == 1 {
 		price *= 2
 	}
-	return spaceline, int(price)
+
+	// Spaceline
+	switch rand.Intn(2) {
+	case 0:
+		fmt.Print("Virgin Galactic")
+	case 1:
+		fmt.Print("SpaceX")
+	case 2:
+		fmt.Print("Space Adventures")
+	}
+
+	fmt.Print("\t")
+
+	// Days
+	fmt.Print(rand.Intn(30) + 1)
+	fmt.Print("\t")
+
+	// Trip type
+	if rand.Intn(1) == 1 {
+		fmt.Print("One-way")
+	} else {
+		fmt.Print("Round-trip")
+		price *= 2
+	}
+	fmt.Print("\t")
+
+	// Price
+	fmt.Print("$ ")
+	fmt.Println(int(price))
 }
 
-func printTable() {
+func printTableHeader() {
 	fmt.Println("Spaceline\tDays\tTrip type\tPrice")
 	fmt.Println("=============================================")
 }
 
 func main() {
 
-	printTable()
+	printTableHeader()
 
-	fmt.Print("$ ")
-	fmt.Print(speed)
+	for i := 0; i < 10; i++ {
+		generateTicket()
+	}
 }
